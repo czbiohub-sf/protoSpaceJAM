ba1d312758600256d5f6c9a0e1c51742aa4c6965
duopeng <pengxunduo@gmail.com>
2022-05-18 02:14:16 +0000

    add serilzied fa

---
+++ genome_files/hg38_byChr/load_fa.py
@@ -0,0 +1,80 @@
+import os.path
+import pandas as pd
+from Bio import SeqIO
+import csv
+import argparse
+import sys
+import linecache
+import datetime
+import gzip
+import shutil
+import gc
+import re
+
+class MyParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
+
+def parse_args():
+    parser= MyParser(description='This script get all the gRNA in the human genome, please specify either --fastagz or --fasta')
+    parser.add_argument('--fastagz', default="", type=str, help='path to the human genome fasta file', metavar='')
+    config = parser.parse_args()
+    if len(sys.argv)==1: # print help message if arguments are not valid
+        parser.print_help()
+        sys.exit(1)
+    return config
+
+config = vars(parse_args())
+
+#####################
+##      main       ##
+#####################
+def main():
+    try:
+        #check input
+        if (config["fastagz"] is None or config["fastagz"] == "") and (config["fasta"] is None or config["fasta"] == ""):
+            log.error(f"please specify either --fastagz or --fasta")
+            sys.exit("Please fix the error(s) above and rerun the script")
+
+        starttime = datetime.datetime.now()
+        seq_count = 0
+
+
+        infile = config["fastagz"]
+        if infile is None or infile == "":
+            infile = config["fasta"]
+
+        with gzip.open(infile, "rt") as f:
+            #wfh.write(f"seq_id\tlength\n")#header
+            for record in SeqIO.parse(f, "fasta"):
+                seq_count+=1
+                #print(f"chr:\t{record.id}\tlength:\t{len(record.seq)}\n}", flush=True)
+
+        endtime = datetime.datetime.now()
+        elapsed_sec = endtime - starttime
+        elapsed_min = elapsed_sec.seconds / 60
+        #log.info(f"finished in {elapsed_min:.2f} min, processed {seq_count} seqs")
+        print(f"finished in {elapsed_min:.2f} min ({elapsed_sec} sec), processed {seq_count} seqs")
+
+    except Exception as e:
+        print("Unexpected error:", str(sys.exc_info()))
+        print("additional information:", e)
+        PrintException()
+
+##########################
+## function definitions ##
+##########################
+def PrintException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
+
+
+if __name__ == "__main__": main()
+
