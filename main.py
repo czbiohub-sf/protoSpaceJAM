31d91e02296079cf1fd6a858db1f07b210540b3d
Duo Peng <pengxunduo@gmail.com>
2022-05-17 19:13:46 -0700

    add input

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ class MyParser(argparse.ArgumentParser):
 def parse_args():
     parser= MyParser(description='This scripts creates a mapping of ENST to chr from gff3')
     parser.add_argument('--genome_ver', default="hg38", type=str, help='pickle file containing the ENST_info dict', metavar='')
+    parser.add_argument('--path2csv', default="input.csv", type=str,
+                        help='path to a csv file containing ENST information\n *required columns*: Ensemble_ID',
+                        metavar='')
     config = parser.parse_args()
     return config
 
@@ -42,23 +45,34 @@ def main():
     try:
         starttime = datetime.datetime.now()
 
-        #load gene model
+        #load gene model info
         ENST_info = read_pickle_files(os.path.join("genome_files","gff3",config['genome_ver'],"ENST_info.pickle"))
 
-        #load gRNA file
+        #load gRNA info index (loc-to-filename)
         loc2file_index = read_pickle_files(os.path.join(f"gRNA_{config['genome_ver']}","loc2file_index.pickle"))
 
         #load ENST list
+        df = pd.read_csv(os.path.join(config['path2csv']))
+        # check csv columns
+        keys2check = set(["Ensemble_ID"])
+        if not keys2check.issubset(df.columns):
+            log.error(f"Missing columns in the input csv file\n Required columns:\"Ensemble_ID\"")
+            log.info(f"Please fix the input csv file and try again")
+            sys.exit()
 
         #loop through each ENST
+        for index, row in df.iterrows():
+            ENST_ID = row["Ensemble_ID"]
+            print(f"processing {ENST_ID}", flush=True)
 
         #write csv out
 
         endtime = datetime.datetime.now()
         elapsed_sec = endtime - starttime
         elapsed_min = elapsed_sec.seconds / 60
-        log.info(f"finished in {elapsed_min:.2f} min ({elapsed_sec} sec) , pickle file(s) loaded")
+
         print(f"finished in {elapsed_min:.2f} min ({elapsed_sec} sec), pickle file(s) loaded", flush=True)
+        log.info(f"finished in {elapsed_min:.2f} min ({elapsed_sec} sec) , pickle file(s) loaded")
 
     except Exception as e:
         print("Unexpected error:", str(sys.exc_info()))
@@ -73,8 +87,34 @@ def read_pickle_files(file):
         mydict = pickle.load(handle)
     return mydict
 
-#def read_csv(file):
-#    with open(file,"r") as handle:
+def get_gRNAs(ENST_ID,ENST_info):
+
+
+
+
+    print("checkpoint")
+
+def get_start_stop_loc(ENST_ID,ENST_info)
+    my_transcript = ENST_info[ENST_ID]  # get the seq record
+    # constructing the list of cds
+    cdsList = [feat for feat in my_transcript.features if feat.type == 'CDS']
+    CDS_first = cdsList[0]
+    CDS_last = cdsList[len(cdsList) - 1]
+    #get start codon location
+    if CDS_first.strand==1:
+        ATG_loc = [CDS_first.location.start,CDS_first.location.start+2, 1] # format [start, end, strand]
+    else:
+        ATG_loc = [CDS_first.location.end-2, CDS_first.location.end, -1]
+    #get stop codon location
+    if CDS_last.strand==1:
+        stop_loc = [CDS_first.location.end-2,CDS_first.location.end, 1]
+    else:
+        stop_loc = [CDS_first.location.start,CDS_first.location.start+2, -1]
+
+def get_seq(chr,start,stop)
+
+
+
 
 
 def PrintException():
