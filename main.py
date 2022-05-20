3ccc48f0a100279140fd8199ff0c24d69f6ecebf
Duo Peng <pengxunduo@gmail.com>
2022-05-19 18:04:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -45,14 +45,15 @@ config = vars(parse_args())
 def main():
     try:
         starttime = datetime.datetime.now()
+        freq_dict = dict()
 
         #load gene model info
         ENST_info = read_pickle_files(os.path.join("genome_files","gff3",config['genome_ver'],"ENST_info.pickle"))
 
-        #load gRNA info index (loc-to-filename)
+        #load gRNA info index (mapping of chromosomal location to file parts)
         loc2file_index = read_pickle_files(os.path.join(f"gRNA_{config['genome_ver']}","loc2file_index.pickle"))
 
-        #load ENST list
+        #load ENST list (the user input list)
         df = pd.read_csv(os.path.join(config['path2csv']))
         # check csv columns
         keys2check = set(["Ensemble_ID"])
@@ -65,7 +66,7 @@ def main():
         for index, row in df.iterrows():
             ENST_ID = row["Ensemble_ID"]
             print(f"processing {ENST_ID}", flush=True)
-            gRNAs = get_gRNAs(ENST_ID,ENST_info)
+            gRNAs = get_gRNAs(ENST_ID = ENST_ID, ENST_info= ENST_info, freq_dict = freq_dict, loc2file_index= loc2file_index, dist = 100)
 
         #write csv out
 
@@ -89,16 +90,35 @@ def read_pickle_files(file):
         mydict = pickle.load(handle)
     return mydict
 
-def get_gRNAs(ENST_ID,ENST_info):
+def get_gRNAs(ENST_ID, ENST_info, freq_dict, loc2file_index, dist = 100):
+    """
+    input
+        ENST_ID:
+        ENST_info:
+    return:
+        a dictionary of guide RNAs which cuts <[dist] to end of start codon
+        a dictionary of guide RNAs which cuts <[dist] to start of stop codon
+    """
+    #get location of start and stop location
     ATG_loc, stop_loc = get_start_stop_loc(ENST_ID,ENST_info)
 
+    #get start codon seq (for debug)
     seq = get_seq(ATG_loc[0],ATG_loc[1],ATG_loc[2],ATG_loc[3])
-    print (ATG_loc)
-    print (f"start codon {seq}")
+    #print (ATG_loc)
+    #print (f"start codon {seq}")
+    update_dict_count(seq,freq_dict)
 
+    #get the exact stop codon (for debug)
     seq = get_seq(stop_loc[0],stop_loc[1],stop_loc[2],stop_loc[3])
-    print (stop_loc)
-    print(f"stop codon {seq}")
+    #print (stop_loc)
+    #print(f"stop codon {seq}")
+    update_dict_count(seq, freq_dict)
+
+    #get gRNAs around the start codon
+    print(ATG_loc)
+    end_of_ATG_loc = [ATG_loc[0],ATG_loc[2],ATG_loc[3]] # [chr, pos,strand]
+    get_gRNAs_near_loc(loc=end_of_ATG_loc,dist=100, loc2file_index=loc2file_index)
+    #get gRNAs around the stop codon
 
 def get_start_stop_loc(ENST_ID,ENST_info):
     my_transcript = ENST_info[ENST_ID]  # get the seq record
@@ -119,6 +139,59 @@ def get_start_stop_loc(ENST_ID,ENST_info):
 
     return([ATG_loc, stop_loc])
 
+def get_gRNAs_near_loc(loc,dist, loc2file_index):
+    """
+    input
+        loc: [chr,pos,strand]
+        dist: distance
+    return:
+        a dataframe of guide RNAs which cuts <[dist] to the loc
+    """
+    chr = loc[0]
+    pos = loc[1]
+    chr_dict = loc2file_index[chr]
+    target_files =[] # a list of file names containing gRNAs near loc
+    #lookup the file
+    for key in chr_dict.keys():
+        file_start = key.split("-")[0]
+        file_end =  key.split("-")[1]
+        interval = [file_start, file_end]
+        if in_interval(pos, interval) or in_interval(pos-1000, interval) or in_interval(pos+1000, interval):
+            target_files.append(chr_dict[key])
+    #print(target_files)
+    #load the gRNAs in the file
+    dfs =[]
+    for file in target_files:
+        file_path = os.path.join(f"gRNA_{config['genome_ver']}","gRNA.tab.gz.split.BwaMapped.scored",file)
+        df_tmp = pd.read_csv(file_path, sep="\t", compression='infer', header=None, names = ["seq","pam","start","end", "strand", "CSS", "ES"])
+        dfs.append(df_tmp)
+    df_gRNA = pd.concat(dfs)
+    #subset gRNA, get gRNAs which cuts <[dist] to the loc
+
+def in_interval(pos,interval):
+    """
+    check if pos in is interval
+    input
+        pos
+        interval: [start,end]
+    return: boolean
+    """
+    pos = int(pos)
+    interval[0] = int(interval[0])
+    interval[1] = int(interval[1])
+    if pos >= interval[0] and pos <= interval[1]:
+        print(f"{pos} is in {interval}")
+        return True
+    else:
+        return False
+
+def update_dict_count(key,dict): # update the dictionary that keeps the count of each string (key)
+    if key in dict.keys():
+        dict[key]+=1
+    else:
+        dict[key]=1
+    return dict
+
 def get_seq(chr,start,end,strand):
     '''
     chr
@@ -138,8 +211,6 @@ def get_seq(chr,start,end,strand):
             return(subseq)
     else:
         sys.exit(f"ERROR: file not found: {chr_file_path}")
-
-
 
 
 def PrintException():
